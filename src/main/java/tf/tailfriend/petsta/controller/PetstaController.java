c6f9cc2007bfcae7326bf4a2651a3a0f928616bf
c4t4 <122370022+C4T4767@users.noreply.github.com>
2025-04-17 18:06:17 +0900

    Update PetstaController.java

--- src/main/java/tf/tailfriend/petsta/controller/PetstaController.java
+++ src/main/java/tf/tailfriend/petsta/controller/PetstaController.java
@@ -1,4 +1,15 @@
 package tf.tailfriend.petsta.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/petsta")
 public class PetstaController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
 }
